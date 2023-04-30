7ff7bc42b2bd86b670c51ac084cfc4134df1f1ee
Inna Fisyuk <inna.fisyuk.0203@gmail.com>
2023-04-30 11:34:59 +0300

    Добавьте файлы проекта.

---
+++ CriticalSection/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace CriticalSection
+{
+    class Program
+    {
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int t = Convert.ToInt32(Console.ReadLine());
+            Thread[] threads = new Thread [t];
+
+            Stopwatch sw = new Stopwatch();//--------------------время- доделать
+            sw.Start();
+            for (int i=0; i< t; i++)
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(IterL));
+            }
+
+            for (int i = 0; i < t; i++)
+            {
+                threads[i].Start(x);
+            }
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+        }
+
+        public static void IterL(object X)
+        {
+            int x = (Int32)X;
+
+            lock (locker)
+            {
+                for(int i=0; i< 10; i++)
+                {
+                    ++x;
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "  " + x);
+                }
+            }
+        }
+
+        public static void IterWL(object X)
+        {
+            int x = (Int32)X;
+            
+                for (int i = 0; i < 10; i++)
+                {
+                    ++x;
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "  " + x);
+                }
+            
+
+        }
+
+    }
+}
